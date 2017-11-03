710878be83c34f3417c41aaf0cd28a1765fbaa43
Connor Boyle <connor.bo@gmail.com>
2017-11-02 19:45:10 -0500

    Change to Backwards-compatible subprocess Method

--- driver.py
+++ driver.py
@@ -14,9 +14,9 @@ output.writerow(['']+[str(i) for i in PROBLEM_SIZES])
 for num_threads in NUM_THREADS:
     row = [str(num_threads)]
     for problem_size in PROBLEM_SIZES:
-        result = subprocess.run(['mpirun', '-np', str(num_threads), '--map-by', 'node', 'mergeSortMPI', str(problem_size)], stdout=subprocess.PIPE)
+        result, error = subprocess.Popen(['mpirun', '-np', str(num_threads), '--map-by', 'node', 'mergeSortMPI', str(problem_size)], stdout=subprocess.PIPE).communicate()
+        row += [result.decode('utf-8').strip()]
         print("Finished problem size: %s threads: %s" % (problem_size, num_threads))
-        row += [result.stdout.decode('utf-8').strip()]
     output.writerow(row)
 
 output_file.close()
